5323652facbc1a853701752c77eabf4de9b02ae4
Dattn0801 <thanhdat08011999@gmail.com>
2021-10-08 14:11:47 +0700

    Lap2 done

---
+++ Lap2/StrategyPattern2/StrategyPattern2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace StrategyPattern2
+{
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Behavior abstract
+        public abstract class VehicaleBehavior
+        {
+            public abstract string AboutMe(string vehicle);
+        }
+
+        // can fly
+        class FlyBehavior : VehicaleBehavior
+        {
+            public override string AboutMe(string vehicle)
+            {
+                return vehicle + " can fly  currently";
+            }
+        }
+
+        // Cant float
+        class FloatBehavior : VehicaleBehavior
+        {
+            public override string AboutMe(string vehicle)
+            {
+                return vehicle+ " can float  currently";
+            }
+        }
+
+        // Just create,haven't done anything yet
+
+        class InitialBehavior : VehicaleBehavior
+        {
+            public override string AboutMe(string vehicle)
+            {
+                return vehicle +"  just create,haven't done anything yet";
+            }
+        }
+
+        public class Vehicle
+        {
+            VehicaleBehavior behavior;
+            string vehicleType;
+            public Vehicle(string vehicleType)
+            {
+                this.vehicleType = vehicleType;
+                this.behavior = new InitialBehavior();
+            }
+            public void setVehicleBehavior(VehicaleBehavior behavior)
+            {
+                this.behavior = behavior;
+            }
+            public string DisplayAboutMe()
+            {
+                return this.behavior.AboutMe(this.vehicleType);
+            }
+        }
+        Vehicle KOto = new Vehicle("Car of K");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = KOto.DisplayAboutMe();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            KOto.setVehicleBehavior(new FlyBehavior());
+            textBox1.Text = KOto.DisplayAboutMe();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            KOto.setVehicleBehavior(new FloatBehavior());
+            textBox1.Text = KOto.DisplayAboutMe();
+        }
+    }
+}
---
+++ Lap2/StrategyPattern2/StrategyPattern2/StrategyPattern.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyPattern2
+{
+    class StrategyPattern
+    {
+        // Behavior abstract
+        public abstract class VehicaleBehavior
+        {
+            public abstract string AboutMe(string vehicle);
+        }
+
+        // can fly
+        class FlyBehavior : VehicaleBehavior
+        {
+            public override string AboutMe(string vehicle)
+            {
+                return vehicle;
+            }
+        }
+
+        // Cant float
+        class FloatBehavior : VehicaleBehavior
+        {
+            public override string AboutMe(string vehicle)
+            {
+                return vehicle;
+            }
+        }
+
+        // Just create,haven't done anything yet
+
+        class InitialBehavior : VehicaleBehavior
+        {
+            public override string AboutMe(string vehicle)
+            {
+                return vehicle;
+            }
+        }
+
+        public class Vehicle
+        {
+            VehicaleBehavior behavior;
+            string vehicleType;
+            public Vehicle(string vehicleType)
+            {
+                this.vehicleType = vehicleType;
+                this.behavior = new InitialBehavior();
+            }
+            public void setVehicleBehavior(VehicaleBehavior behavior)
+            {
+                this.behavior = behavior;
+            }
+            public string DisplayAboutMe()
+            {
+                return this.behavior.AboutMe(this.vehicleType);
+            }
+        }
+    }
+}
