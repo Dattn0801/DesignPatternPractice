da44866e9a140be32dcd382052521e1450d79e9b
Dattn0801 <thanhdat08011999@gmail.com>
2021-11-06 15:31:45 +0700

    lap 6

---
+++ Lap6/Template Method/Template Method/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+            Console.WriteLine("------------------------------");
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+            Console.WriteLine("------------------------------");
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lap6/Template Method/Template Method/WholeWheat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Method
+{
+    class WholeWheat : Bread
+    {
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the wholewheat Bread.(15 minutes) ");
+        }
+
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for WholeWheat Bread.");
+        }
+    }
+}
